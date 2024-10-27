0943a73d8c0c5f3a840504d7adf5b8e25509baaa
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-10-27 18:21:22 +0100

    Creation debut projet

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <main>
+        <AddToCart />
+    </main>
+  )
+}
+
+export default ProductCard
