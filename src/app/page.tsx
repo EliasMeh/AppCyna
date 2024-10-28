d74d66e125d85d32e69c9999696014ad46280caf
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-10-28 08:34:20 +0100

    28/10/2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import ProductCard from "./components/ProductCard";
 import UsersPage from "./users/page";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -9,9 +10,14 @@ export default function Home() {
       <div>
         <h1>Hello World</h1>
         <ProductCard />
-        <Link href="/users">
-          <button>Go to Users Page</button>
-        </Link>
+        <div>
+          <Link href="/users">
+            <button>Go to Users Page</button>
+          </Link>
+        </div>
+        <div>
+          <Button variant="outline">Click Me</Button>
+        </div>
       </div>
     </main>
   );
---
+++ src/lib/utils.tsx
@@ -0,0 +1,7 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// A utility function for merging Tailwind CSS classes with clsx and tailwind-merge
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
