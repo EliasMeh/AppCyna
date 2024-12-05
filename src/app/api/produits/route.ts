3cae2237651f8a08d1ea30c7b67b94833214480e
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-12-05 11:30:37 +0100

    Suppression de l'api pour mise en prod

--- src/app/api/produits/route.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default async function handle(req, res) {
-  const produits = await prisma.produit.findMany();
-  res.json(produits);
-}
