d227058fd6da0124b37b80d226535c87374597b3
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2025-01-27 15:19:25 +0100

    Résolution du problème(ajout Suspense)

--- src/app/pages/recherche/page.tsx
+++ src/app/pages/recherche/page.tsx
@@ -2,15 +2,25 @@ import Footer from '@/app/communs/Footer'
 import Header from '@/app/communs/Header'
 import React from 'react'
 import Search from '@/app/components/Search'
+import { Suspense } from 'react';
 
 const page = () => {
   return (
     <main>
+        
         <Header />
+        <div className='pl-2'>
         <h1>Recherche</h1>
         <p>Page de recherche</p>
-        <Search/>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Search/>
+        </Suspense>
+        <div className='mt-60'>
+
+        </div>
+        </div>
         <Footer />
+        
     </main>
   )
 }
